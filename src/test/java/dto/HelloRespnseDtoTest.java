d25286d5475accc79c9b68234c2cefb009ef4854
Radline000 <radline000@gmail.com>
2020-09-23 23:55:27 +0900

    build 수정1

---
+++ src/test/java/dto/HelloRespnseDtoTest.java
@@ -0,0 +1,22 @@
+package dto;
+import com.jojoidu.book.springboot.web.HelloController;
+import com.jojoidu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloRespnseDtoTest {
+    @Test
+    public void 롬북_기능_테스트(){
+        //given
+        String name="test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
