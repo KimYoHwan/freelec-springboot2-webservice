d7d989daa041d0774a6afcd9adf1c60a2b8f9b89
Radline000 <radline000@gmail.com>
2020-09-24 05:59:38 +0900

    application-real.properties 설정

--- src/main/java/com/jojoidu/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/jojoidu/book/springboot/domain/user/UserRepository.java
@@ -1,5 +1,5 @@
 package com.jojoidu.book.springboot.domain.user;
-import com.jojoidu.book.springboot.domain.user.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
