80545efa75b00917a7c0fb597bc18d9a4196d73e
Radline000 <radline000@gmail.com>
2020-09-30 04:35:30 +0900

    무중단 배포

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=freelec-springboot2-webservice
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -19,7 +19,7 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
